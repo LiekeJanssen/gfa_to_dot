5dbd7ff1b429f7f014ef829e691305fb8aa4a015
ljanssen <ljanssen@ljanssen01.phage.bcgsc.ca>
2018-10-30 14:10:25 -0700

    remove unused imports

--- src/gfa/GFA.java
+++ src/gfa/GFA.java
@@ -1,14 +1,8 @@
 package gfa;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import javax.sound.midi.MidiDevice.Info;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.InputStreamReader;
 
 /**
  *
